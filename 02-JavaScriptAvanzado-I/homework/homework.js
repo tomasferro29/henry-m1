db8210996b67df0eb1d39e5cf538a1884cc3409a
Tomas Ferro <tomasferro29@gmail.com>
2023-07-20 16:45:23 -0300

    02

---
+++ 02-JavaScriptAvanzado-I/homework/homework.js
@@ -0,0 +1,70 @@
+// x = 1;
+// var a = 5;
+// var b = 10;
+// var c = function(a, b, c) {
+//   var x = 10;
+//   console.log(x);
+//   console.log(a);
+//   var f = function(a, b, c) {
+//     b = a;
+//     console.log(b);
+//     b = c;
+//     var x = 5;
+//   }
+//   f(a,b,c);
+//   console.log(b);
+// }
+// c(8,9,10);
+// console.log(b);
+// console.log(x);
+
+// console.log(bar);
+// console.log(baz);
+// foo();
+// function foo() { console.log('Hola!'); }
+// var bar = 1;
+// baz = 2;
+
+// var instructor = "Tony";
+// if(true) {
+//     var instructor = "Franco";
+// }
+// console.log(instructor);
+
+// var instructor = "Tony";
+// console.log(instructor);
+// (function a() {
+//    if(true) {
+//       var instructor = "Franco";
+//       console.log(instructor);
+//    }
+// })();
+// console.log(instructor);
+
+// var instructor = "Tony";
+// let pm = "Franco";
+// if (true) {
+//     var instructor = "The Flash";
+//     let pm = "Reverse Flash";
+//     console.log(instructor);
+//     console.log(pm);
+// }
+// console.log(instructor);
+// console.log(pm);
+
+console.log(6 / "3")
+console.log("2" * "3")
+console.log(4 + 5 + "px")
+console.log("$" + 4 + 5)
+console.log("4" - 2)
+console.log("4px" - 2)
+console.log(7 / 0)
+console.log({}[0])
+console.log(parseInt("09"))
+console.log(5 && 2)
+console.log(2 && 5)
+console.log(5 || 0)
+console.log(1 || 5)
+console.log([3]+[3]-[10])
+console.log(3>2>1)
+console.log([] == ![])
